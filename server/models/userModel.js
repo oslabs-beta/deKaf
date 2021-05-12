026a7e4259924139a7a97aafc37aca59810d2176
mikeFeldman15 <michaelfeldma1@gmail.com>
2021-05-12 14:49:50 -0400

    setting up backend boiler plate

---
+++ server/models/userModel.js
@@ -0,0 +1,37 @@
+const { Pool } = require("pg");
+require('dotenv').config();
+
+/*Schema set up as below - used CREATE TABLE in ElephantSQL to make the schema. Syntax used listed below:
+CREATE TABLE faves (
+image TEXT,
+commonname TEXT,
+scientific TEXT,
+edible BOOLEAN,
+vegetable TEXT,
+notes TEXT
+);
+*/
+
+
+//URI to elephantSQL database that will store the users favorite plants and any notes that add to those faves
+const PG_URI = process.env.pgURI;
+
+//create a new pool here using the connection string above
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+// We export an object that contains a property called query,
+// which is a function that returns the invocation of pool.query() after logging the query
+// This will be required in the controllers to be the access point to the database
+
+// exporting module with some console logs
+module.exports = {
+  query: (text, params, callback) => {
+    // console.log("executed query", text);
+    params = pool.connectionString;
+    // console.log("executed params", params);
+    // console.log("executed callback", callback);
+    return pool.query(text, params, callback);
+  },
+};
