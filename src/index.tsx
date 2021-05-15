d535d7409ea95d759d984e811963ff7816bfb05c
bulknskull <bhepfing@gmail.com>
2021-05-14 20:28:31 -0400

    built login page, implemented Sass, initial styling/design

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom'; 
 import App from './components/App.tsx';
 // import styles for webpack, this is where we'd import a logo, and append it
-import './styles.css';
+import './styles.scss';
 
 render(<App />, document.getElementById('root'));
