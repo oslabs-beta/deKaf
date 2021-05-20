866d1f1d76cbf4ea86eb568cf146f796cbe9aa1b
bulknskull <bhepfing@gmail.com>
2021-05-20 15:20:49 -0400

    updated copy on home page

--- src/components/Base/Home.tsx
+++ src/components/Base/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div id='home-wrapper'>
             <div className='banner'>
                 <div className='logo'><img height='100%' src='https://i.imgur.com/nWW4UjX.png'/></div>
-                <div className='logo-caption'>A visualization tool for Kafka consumer metrics</div>
+                <div className='logo-caption'>A web-based visualization tool for Kafka consumer metrics</div>
                 <div id='home-options'>
                     <Link to='/about'>
                         <button id='learn-button'>
@@ -28,19 +28,19 @@ const Home = () => {
             
             <div id='home-info'>
                 <div className='home-info-box'>
-                    <div className='home-info-icon'><img width='50%' src='https://i.imgur.com/7H2M39A.png'/></div>
-                    <h3>Visual</h3>
-                    <p> Lorem ipsum etc.</p>
+                    <div className='home-info-icon'><img min-width='80px' width='50%' src='https://i.imgur.com/7H2M39A.png'/></div>
+                    <h3>Intuitive</h3>
+                    <p>Easily view key metrics on your Kafka instance with our intuitive web GUI.</p>
                 </div>
                 <div className='home-info-box'>
-                    <div className='home-info-icon'><img width='50%' src='https://i.imgur.com/zAAVWb0.png'/></div>
+                    <div className='home-info-icon'><img min-width='80px' width='50%' src='https://i.imgur.com/zAAVWb0.png'/></div>
                     <h3>Lightweight</h3>
-                    <p>Can I offer you a nice egg in this trying time?</p>
+                    <p>No installation required; just enter your information and start using deKaf!</p>
                 </div>
                 <div className='home-info-box'>
-                    <div className='home-info-icon'><img width='50%' src='https://i.imgur.com/YOFpmB9.png'/></div>
+                    <div className='home-info-icon'><img min-width='80px' width='50%' src='https://i.imgur.com/YOFpmB9.png'/></div>
                     <h3>Real-time</h3>
-                    <p>Arma virumque cano...</p>
+                    <p>Metrics update automatically as long as you're signed in, so you won't miss a beat.</p>
                 </div>
             </div>
         </div>
