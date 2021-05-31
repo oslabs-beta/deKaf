7733164d95bbefc61f530950502621399de30687
bulknskull <bhepfing@gmail.com>
2021-05-31 16:19:23 -0400

    began adjusting user overview design; added enzyme tests to current version

--- src/components/UserPage/BrokerOverview.tsx
+++ src/components/UserPage/BrokerOverview.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 // @ts-ignore
 import BrokerCard from './BrokerCard.tsx';
+// @ts-ignore 
+import AddTopicCard from './AddTopicCard.tsx';
 
 const BrokerOverview = () => {
+
+  const [topics, setTopics] = useState(1);
+
+  const topicsArray = [];
+
+  for (let i = 0; i < topics; i += 1) {
+    console.log(i);
+  }
 
   return (
     <div>
       <h2 className='gallery-header' id='home-header'>Broker overview</h2>
+      <div id='port-input-container'>
+        This is where they'll input their server port
+      </div>
       <div className='cards-container'>
-        <BrokerCard />
-        <BrokerCard />
-        <BrokerCard />
-        <BrokerCard />
-        <BrokerCard />
-        <div id='add-new-broker-container'>
-          <button>+ Add new broker</button>
+        {topicsArray}
+        <div id='add-new-topic-container'>
+          <AddTopicCard />
         </div>
       </div>
     </div>
